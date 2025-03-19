b7f38d817d36d22107a3dbdda1eda0101485e999
Ashwini12S428 <Ashwini@klebcahubli.in>
2025-03-19 11:34:48 +0530

    finial change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const{User}= require (`./model/User`);
 const mongoose = require('mongoose');
 const cors =require('cors');
 const morgan =require('morgan');
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const jwt =require('jsonwebtoken');
 const{Product}=require('./model/Product');
 const {Cart}=require ('./model/Cart');
@@ -14,9 +14,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+//BQirrDGLBsjygT3A
 
+let MONGODB_URL ="mongodb+srv://ashwini:BQirrDGLBsjygT3A@cluster0.akw98.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
